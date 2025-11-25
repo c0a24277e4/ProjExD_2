12435c2454c22e47ddaa08fa54920ce1fade2293
c0a24277e4 <c0a24277e4@edu.teu.ac.jp>
2025-11-25 14:16:01 +0900

    練習2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,4 +1,5 @@
 import os
+import random
 import sys
 import pygame as pg
 
@@ -20,6 +21,14 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+    bb_img = pg.Surface((20, 20)) #空のSurface
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10) #半径10の赤い円を描画
+    bb_img.set_colorkey((0, 0, 0)) #黒色の透過色の背景
+    bb_rct = bb_img.get_rect() #爆弾rect
+    bb_rct.centerx = random.randint(0, WIDTH) #爆弾横座標
+    bb_rct.centery = random.randint(0, HEIGHT) #爆弾縦座標
+    vx, vy = +5, +5 # 爆弾の横速度，縦速度
+    
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -27,6 +36,7 @@ def main():
             if event.type == pg.QUIT: 
                 return
         screen.blit(bg_img, [0, 0]) 
+        screen.blit(bb_img, bb_rct)
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
@@ -47,6 +57,8 @@ def main():
         
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx, vy)
+        screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
